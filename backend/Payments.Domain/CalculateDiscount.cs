b0f360b89bd8617313be90c7a8e4d4a97a95c930
cyberinnovator <coder@coder.com>
2026-01-06 13:25:05 +0530

    Implemented cardtype detection and discounted and final amount calculation

--- backend/Payments.Domain/CalculateDiscount.cs
+++ backend/Payments.Domain/CalculateDiscount.cs
@@ -5,14 +5,45 @@ namespace Payments.Domain
     public class CalculateDiscount
     {
         public static CardType GetCardType(string cardNumber)
-        {
+        {   
+            if (cardNumber.StartsWith("4"))
+                return CardType.Visa;
+
+            if (cardNumber.StartsWith("5"))
+                return CardType.MasterCard;
+
+            if (cardNumber.StartsWith("6"))
+                return CardType.RuPay;
+
             return CardType.RuPay;
         }
 
         public static (bool DiscountApplied, decimal FinalAmount) Calculate(CardType cardType, decimal amount)
         {
 
-            return (true, 0);
+            decimal discountPercentage = 0;
+
+            switch (cardType)
+            {
+                case CardType.Visa:
+                    discountPercentage = 0;
+                    break;
+
+                case CardType.MasterCard:
+                    discountPercentage = 5;
+                    break;
+
+                case CardType.RuPay:
+                    discountPercentage = 10;
+                    break;
+            }
+
+            decimal discountAmount = amount * discountPercentage / 100;
+            decimal finalAmount = amount - discountAmount;
+
+            bool discountApplied = discountPercentage > 0;
+
+            return (discountApplied, finalAmount);
         }
     }
 }
